ef874589085658acc902cf23086a3a0340be1ffd
Marcin <m.niemirski@time4.digital>
2021-11-28 21:50:10 +0100

    Fix error

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const path = require('path')
+const router = express.Router()
 const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 const Book = require('../models/book')
 const Author = require('../models/author')
-const router = express.Router()
 const uploadPath = path.join('public', Book.coverImageBasePath)
-const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg']
 const upload = multer({
   dest: uploadPath,
   fileFilter: (req, file, callback) => {
@@ -13,37 +14,63 @@ const upload = multer({
   },
 })
 
-// All books route
+// All Books Route
 router.get('/', async (req, res) => {
-  res.send('All books')
-  // res.redirect('books')
+  let query = Book.find()
+  if (req.query.title != null && req.query.title != '') {
+    query = query.regex('title', new RegExp(req.query.title, 'i'))
+  }
+  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+    query = query.lte('publishDate', req.query.publishedBefore)
+  }
+  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+    query = query.gte('publishDate', req.query.publishedAfter)
+  }
+  try {
+    const books = await query.exec()
+    res.render('books/index', {
+      books: books,
+      searchOptions: req.query,
+    })
+  } catch {
+    res.redirect('/')
+  }
 })
 
-// New book role
+// New Book Route
 router.get('/new', async (req, res) => {
   renderNewPage(res, new Book())
 })
 
-// Create book
+// Create Book Route
 router.post('/', upload.single('cover'), async (req, res) => {
   const fileName = req.file != null ? req.file.filename : null
-  // const { title, author, publishDate, pageCount, description } = req.body
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
-    pageCount: req.body.pageCount,
+    pageCount: parseInt(req.body.pageCount),
     coverImageName: fileName,
     description: req.body.description,
   })
+
   try {
     const newBook = await book.save()
     // res.redirect(`books/${newBook.id}`)
-    res.redirect('books')
-  } catch (error) {
+    res.redirect(`books`)
+  } catch {
+    if (book.coverImageName != null) {
+      removeBookCover(book.coverImageName)
+    }
     renderNewPage(res, book, true)
   }
 })
+
+function removeBookCover(fileName) {
+  fs.unlink(path.join(uploadPath, fileName), (err) => {
+    if (err) console.error(err)
+  })
+}
 
 async function renderNewPage(res, book, hasError = false) {
   try {
@@ -58,4 +85,5 @@ async function renderNewPage(res, book, hasError = false) {
     res.redirect('/books')
   }
 }
+
 module.exports = router
